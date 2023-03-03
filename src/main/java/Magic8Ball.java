b9aed1beb41a3aad9c3bf3e990b492386aad1f43
Daniel Commins <danel.commins@techelevator.com>
2023-03-03 13:08:54 -0800

    initial draft

---
+++ src/main/java/Magic8Ball.java
@@ -0,0 +1,40 @@
+/*
+ * Create a Magic 8-ball program that gives custom responses to yes or no questions
+ */
+public class Magic8Ball {
+
+    // 1. Make a main method that includes all the steps below
+
+
+    // 2. Get the user to enter a question for the magic 8-ball to answer
+    //    Scanner scannerObject = new Scanner(System.in);
+    //    String userQuestion = scanner.nextLine();
+
+
+    // 3. Make a variable and initialize it to a random number.
+    //
+    //    // Get random a number from: [0, 1, 2, 3]
+    //    int randNumber = new Random().nextInt(4);
+
+
+    // 4. If the random number is 0
+
+        // Tell the user "Yes"
+
+
+    // 5. If the random number is 1
+
+        // Tell the user "No"
+
+
+    // 6. If the random number is 2
+
+        // Tell the user a custom response
+
+
+    // 7. If the random number is 3
+
+        // Tell the user a custom response
+}
+
+
